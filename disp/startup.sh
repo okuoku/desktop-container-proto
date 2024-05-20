9dc1be55a60061827ef7e4c2b489fb9d78db0185
okuoku <mjt@cltn.org>
2024-05-21 04:20:46 +0900

    disp: Adjust container content

--- disp/startup.sh
+++ disp/startup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/opt/weston/lib
+export LD_LIBRARY_PATH=/opt/weston/lib:/opt/PulseAudio/lib
 export XDG_RUNTIME_DIR=/tmp
 mkdir /tmp/.X11-unix
+
+/opt/PulseAudio/bin/pulseaudio --disallow-exit=true \
+  --exit-idle-time=-1 \
+  "--load=module-rdp-sink sink_name=RDPSink" \
+  "--load=module-rdp-source source_name=RDPSource" \
+  "--load=module-native-protocol-unix socket=/tmp/PluseServer auth-anonymous=true" &
+
 exec /opt/weston/bin/weston --shell=rdprail-shell.so --address=0.0.0.0 \
     --port=5699 --xwayland
